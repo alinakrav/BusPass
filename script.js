6ff957d5be6ec1af211e805081f5a8e793a42030
alinakrav <alina14022000@gmail.com>
2022-02-05 04:31:06 -0500

    done

---
+++ script.js
@@ -0,0 +1,23 @@
+var date;
+document.addEventListener('DOMContentLoaded', function() {
+    setTicketTime();
+    setPhoneTime();
+    setTicketDate();
+    setInterval(setTicketTime, 1000);
+    setInterval(setPhoneTime, 30000);
+}, false);
+
+function setTicketTime() {
+    date = new Date();
+    ticketTime.innerText = pad(date.getHours()) + ':' + pad(date.getMinutes()) + ':' + pad(date.getSeconds());
+}
+function setPhoneTime() {
+    phoneTime.innerText = pad(date.getHours()) + ':' + pad(date.getMinutes());
+}
+function setTicketDate() {
+    ticketDate.innerText = date.toLocaleString('en-us', {month: 'short'}) + ' ' + date.getDate() + ', ' + date.getFullYear();
+}
+
+function pad(time) {
+    return ('0' + time).slice(-2);
+}
